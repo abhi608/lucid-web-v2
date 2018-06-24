ac313de3889c0ce1ff8ca61bfee370fca1a9c01b
animeshram <animeshr@iitk.ac.in>
2018-06-24 15:41:00 +0530

    initial commit

---
+++ static/src/components/SearchView.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import AutoComplete from 'material-ui/AutoComplete';
+import SearchBar from 'material-ui-search-bar';
+import Paper from 'material-ui/Paper';
+
+import * as actionCreators from '../actions/auth';
+import { validateEmail } from '../utils/misc';
+
+function mapStateToProps(state) {
+    return {
+        isAuthenticating: state.auth.isAuthenticating,
+        statusText: state.auth.statusText,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators(actionCreators, dispatch);
+}
+
+
+const style = {
+    marginTop: 50,
+    paddingBottom: 50,
+    paddingTop: 25,
+    width: '100%',
+    textAlign: 'center',
+    display: 'inline-block',
+};
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class SearchView extends React.Component {
+
+    constructor(props) {
+    	super(props);
+    	 this.state = {
+             search_phrase: '',
+         };
+         this.changeValue = this.changeValue.bind(this);
+         this.requestSearch = this.requestSearch.bind(this);
+    }
+
+    changeValue(e){
+    	
+    	const value = e;
+    	const nextState = {};
+        nextState['search_phrase'] = value;
+        this.setState(nextState);
+        console.log(this.state)
+    }
+
+    requestSearch(e) {
+        var data = {
+  				search_phrase:this.state.search_phrase,
+  				};
+
+  		var esc = encodeURIComponent;
+		var query = Object.keys(data)
+    				.map(k => esc(k) + '=' + esc(data[k]))
+    				.join('&');
+
+    	console.log(query)
+        fetch("/api/search?"+query, {
+  			method: "GET",
+			});
+    }
+
+
+    render(){
+    	return(
+    		<div className="col-md-6 col-md-offset-3">
+                
+                    
+                        <div className="text-center">
+                            <h2>Lucid-Law</h2>
+                            
+
+                            <SearchBar
+                            	name="search_phrase"
+                            	value={this.state.search_phrase}
+      							onChange={this.changeValue}
+      							onRequestSearch={this.requestSearch}
+      							style={{
+        						margin: '0 auto',
+        						maxWidth: 800
+      							}}
+    						/>
+                            
+
+                        </div>
+            
+                
+
+            </div>
+        );
+
+    }
+}
