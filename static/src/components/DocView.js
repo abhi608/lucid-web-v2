6f21e995ff65cadf8f7459a81abfe612e2315c54
Rahul Kumar <rowda@Rowdas-MacBook-Pro.local>
2018-06-26 07:44:02 +0530

    add backend cites

--- static/src/components/DocView.js
+++ static/src/components/DocView.js
@@ -12,11 +12,19 @@ import { validateEmail } from '../utils/misc';
 import SearchViewResults from './SearchViewResults';
 import CaseCard from './CaseCard'
 
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import CardHeader from '@material-ui/core/CardHeader';
 import Typography from '@material-ui/core/Typography';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+
 
 const styles = {
     card: {
@@ -55,10 +63,45 @@ export default class DocView extends React.Component {
     	super(props);
 	    console.log("init state",this.props.location.state.response);
         this.state = this.props.location.state.response;
+	this.state['open'] = false;
+	this.state['cite_available'] = false;
+	this.state['doc_list'] = [];
+	this.showCites = this.showCites.bind(this);
+    }
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+
+   showCites(e){
+	console.log("cites clicked");
+	var tid = this.state.response_doc.tid;
+	var parentThis = this;
+	var data = {tid: tid};
+	var esc = encodeURIComponent;
+	var query = Object.keys(data)
+			.map(k => esc(k) + '=' + esc(data[k]))
+                        .join('&');
+
+	fetch("/api/get_cites?"+query).then(
+	    (resp) => resp.json()).then(
+		function(data){
+		    console.log(data);
+		    var newState = {}
+		    newState['cite_available'] = data.cite_available;
+		    newState['doc_list'] = data.doc_list;
+		    newState['open'] = true;
+		    parentThis.setState(newState);
+
+		    console.log(this.state);
+	});		
     }
 
 
     render(){
+	const doc_list = this.state.doc_list;
+	const cite_available = this.state.cite_available;
     	return(
             <div style={{paddingBottom: "1.5em", marginTop: 80}} className="col-md-12">
                 <Card style={styles.card}>
@@ -66,41 +109,62 @@ export default class DocView extends React.Component {
                         <Typography style={styles.title} align="center" color="primary" variant="headline">
                             {this.state.response_doc.title}  
                         </Typography>
+			<div className="row">
+			    <div className="col-md-6">
+			    	<Typography style={styles.title} color="textSecondary">
+			    	    Type: {this.state.response_doc.divtype}  
+			    	</Typography>
+			    	<Typography style={styles.title} color="textSecondary">
+			    	    Author: {this.state.response_doc.author}  
+			    	</Typography>
+			    	<Typography style={styles.title} color="textSecondary">
+			    	    Bench: {this.state.response_doc.bench}  
+			    	</Typography>
+			    	<Typography style={styles.title} color="textSecondary">
+			    	    Source: {this.state.response_doc.source}  
+			    	</Typography>
+			    </div>
+			    <div className="col-md-6" styles={{alignItems: 'right', display: 'flex'}}>
+                        <Button  variant="raised" color="primary" onClick={this.showCites}>Show Citations</Button>
+
+			    </div>
+			</div>
                         <Typography style={styles.title} color="textSecondary">
-                            Type: {this.state.response_doc.divtype}  
+                            Query based Summary:<br/> 
+			    {this.state.response_doc.query_summary}  
+                        </Typography>
+			<Typography style={styles.title} color="textSecondary">
+                            Summary:<br/> 
+			    {this.state.response_doc.summary}  
                         </Typography>
                         <Typography style={styles.title} color="textSecondary">
-                            Author: {this.state.response_doc.author}  
+                            Content:<br/>
+ 			    {this.state.response_doc.doc}  
                         </Typography>
-                        <Typography style={styles.title} color="textSecondary">
-                            Bench: {this.state.response_doc.bench}  
-                        </Typography>
-                        <Typography style={styles.title} color="textSecondary">
-                            Source: {this.state.response_doc.source}  
-                        </Typography>
-                        <Typography style={styles.title} color="textSecondary">
-                            Summary: {this.state.response_doc.summary}  
-                        </Typography>
-                        <Typography style={styles.title} color="textSecondary">
-                            Query based Summary: {this.state.response_doc.query_summary}  
-                        </Typography>
-                        <Typography style={styles.title} color="textSecondary">
-                            Content: {this.state.response_doc.doc}  
-                        </Typography>
-                        {/* <Typography variant="headline" component="h2">
-                    {this.props.title}
-                        </Typography>
-                        <Typography style={styles.pos} color="textSecondary">
-                                {this.props.bench},{this.props.source}
-                        </Typography>
-                        <Typography component="p">
-                            {this.props.highlights} 
-                        </Typography> */}
+                        
                     </CardContent>
-                    {/* <CardActions>
-                        <Button size="small" onClick={this.showDocument}>View More</Button>
-                    </CardActions> */}
                 </Card>
+      <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+          <DialogContent>
+		{cite_available ? doc_list.map(function(d, idx){
+			 return (<CaseCard {...ResultsViewThis.props} title={d.title} tid={d.tid} key={d.tid} 
+				bench={d.bench} source={d.source} highlights={d.highlights} doc_type={d.divtype} />)
+		       })
+		:
+		<p>No cited documents available</p>	}
+         </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+          </DialogActions>
+        </Dialog>
+
             </div>
         );
 
