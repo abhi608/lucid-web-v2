15e37ef6535781a3a8a10c55bd0e8076a639e462
Nishit Asnani <asnani04@gmail.com>
2018-07-06 03:35:58 +0530

    routes modified:

--- static/src/routes.js
+++ static/src/routes.js
@@ -12,6 +12,7 @@ import ProtectedView from './components/ProtectedView';
 import Analytics from './components/Analytics';
 import NotFound from './components/NotFound';
 import SearchView from './components/SearchView';
+import HomeView from './components/HomeView';
 import ResultsView from './components/ResultsView';
 import DocView from './components/DocView';
 
@@ -21,15 +22,20 @@ import { requireAuthentication } from './components/AuthenticatedComponent';
 import { requireNoAuthentication } from './components/notAuthenticatedComponent';
 
 export default (
-    <Route path="/" component={App}>
+	<Route>
+	<Route path="/home" component={requireNoAuthentication(HomeView)} />
+	// <Route exact path="/h" render={() => {window.location.href="../homepage/index.html"}} />
+	<Route path="/" component={App}>
         <Route path="main" component={requireAuthentication(ProtectedView)} />
         <Route path="login" component={requireNoAuthentication(LoginView)} />
         <Route path="register" component={requireNoAuthentication(RegisterView)} />
-        <Route path="home" component={requireNoAuthentication(HomeContainer)} />
+        // <Route path="home" component={requireNoAuthentication(HomeContainer)} />
         <Route path="analytics" component={requireAuthentication(Analytics)} />
         <Route path="search" component={requireNoAuthentication(SearchView)} />
         <Route path="results" component={requireNoAuthentication(ResultsView)} />
         <Route path="doc" component={requireNoAuthentication(DocView)} />
         <Route path="*" component={DetermineAuth(NotFound)} />
     </Route>
+	
+	</Route>
 );
